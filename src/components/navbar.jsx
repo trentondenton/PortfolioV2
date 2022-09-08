fa5a4b41cba48a42244ef431ab09ea0effcd629c
Trenton Denton <trentondenton91@gmail.com>
2022-09-08 09:51:13 -0500

    Navbar mouse events, & style changes.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,8 +6,11 @@ import { IconEqualsOutline } from './icons';
 import Logo from '../assets/images/logo.png';
 
 function Navbar() {
-  const [isExpanded, setisExpanded] = useState(false)
-  const expandNav = () => { setisExpanded(!isExpanded) }
+  const [isExpanded, setisExpanded] = useState(false);
+  const expandNav = () => { setisExpanded(!isExpanded) };
+  const handleMouseLeave = (e) => { setisExpanded(false) };
+  const handleMouseEnter = (e) => { setisExpanded(true) };
+
   return (
     <div className="navigation-container">
       <div className="nav-logo">
@@ -16,13 +19,13 @@ function Navbar() {
       <div className="nav-menu">
         <ul>
           {isExpanded ?
-            <nav className="nav-menu-open">
-              <li><HLink to="/#home" onClick={expandNav} smooth>Home</HLink></li>
-              <li><HLink to="/#projects" onClick={expandNav} smooth>Projects</HLink></li>
-              <li><HLink to="/#contact" onClick={expandNav} smooth>Contact</HLink></li>
-              <li><RLink to="/blog" onClick={expandNav} smooth>Blog</RLink></li>
+            <nav className="nav-menu-open" onMouseLeave={handleMouseLeave}>
+              <li><HLink to="/#home" onClick={expandNav}>Home</HLink></li>
+              <li><HLink to="/#projects" onClick={expandNav}>Projects</HLink></li>
+              <li><HLink to="/#contact" onClick={expandNav}>Contact</HLink></li>
+              <li><RLink to="/blog" onClick={expandNav}>Blog</RLink></li>
             </nav>
-            : <IconEqualsOutline onClick={expandNav} />
+            : <IconEqualsOutline onClick={expandNav} onMouseEnter={handleMouseEnter} />
           }
         </ul>
       </div>
